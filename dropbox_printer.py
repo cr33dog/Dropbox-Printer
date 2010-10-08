1d21f95f517abc0075280d82eac301c6b8d9f04e
Chris Mohler <cr33dog@gmail.com>
2010-10-08 14:10:05 -0500

    Using CUPS now instead of lpr.

--- dropbox_printer.py
+++ dropbox_printer.py
@@ -4,9 +4,13 @@
 import os
 import subprocess
 import pyinotify
+import cups
 
 # path to Dropbox Folder
 path = os.path.expanduser('~/Dropbox/print_queue/')
+
+# whether or not to delete files after printing
+empty_queue = True
 
 # file extensions to run through imagemagick
 ext_to_convert = ('svg')
@@ -14,6 +18,8 @@ ext_to_convert = ('svg')
 # arguments to convert command
 convert_args = ['-page','Letter']
 
+# CUPS job options
+cups_options = {}
 
 def check(path):
     if not os.path.exists(path):
@@ -23,7 +29,7 @@ def check(path):
             raise
 
 # based on the pyinotify loop.py example
-def watch(path):
+def watch (path):
     wm = pyinotify.WatchManager()
     handler = EventHandler()
     notifier = pyinotify.Notifier(wm, default_proc_fun=handler)
@@ -31,7 +37,22 @@ def watch(path):
     wm.add_watch(path, mask, rec=False) #rec=False prevents recursion
     notifier.loop()
 
-class EventHandler(pyinotify.ProcessEvent):
+def sendToPrinter (filename, tempname=None):
+    conn = cups.Connection()
+    printer = conn.getDefault()
+    try:
+        if tempname:
+            job = conn.printFile(printer, tempname, '', cups_options)
+            os.remove(tempname)
+        else:
+            job = conn.printFile(printer, filename, '', cups_options)
+    except:
+        raise
+    if empty_queue:
+        os.remove(filename)
+
+
+class EventHandler (pyinotify.ProcessEvent):
     def process_IN_MOVED_TO(self, event):
         if event.dir == False:
             if event.name.split('.')[-1].lower() in ext_to_convert:
@@ -40,12 +61,9 @@ class EventHandler(pyinotify.ProcessEvent):
                 convert_cmd.extend(convert_args)
                 convert_cmd.append(tempname)
                 subprocess.call(convert_cmd)
-                subprocess.call(['lpr', tempname])
-                os.remove(tempname)
-                os.remove(event.pathname)
+                sendToPrinter(event.pathname, tempname)
             else:
-                subprocess.call(['lpr', event.pathname])
-                os.remove(event.pathname)
+                sendToPrinter(event.pathname)
 
 if __name__ == '__main__':
     check(path)
